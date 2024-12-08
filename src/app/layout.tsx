8b349b7cc79a88b3545e7303ad088ed5c9f9987c
Josh Mitteldorf <aging.advice@gmail.com>
2024-12-07 21:27:29 -0500

    google analytics + extended About

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
         </div>
         <Footer/>
       </body>
+      <GoogleAnalytics gaId="G-28J2ZDLR3P" />
     </html>
   );
 }
